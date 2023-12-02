91cab9becc3007670ad379ae8ef9af215ed9afd0
GalaxyDestroyer <zagusopas25@gmail.com>
2023-12-02 13:54:19 +0800

    Try

--- main.py
+++ main.py
@@ -1971,7 +1971,4 @@ def receive_trainimage(image):
 if __name__ == "__main__":
     #app.run()
     #app.run(host='127.0.0.1', port=5000, debug=True)
-    #socketio.run(app, debug=True, port=5000, host='0.0.0.0')
-    #socketio.run(app, port=5000, debug=True, allow_unsafe_werkzeug=True)
-    socketio.run(app, port=20449, debug=True, allow_unsafe_werkzeug=True)
-
+    socketio.run(app, port=20499, debug=True, allow_unsafe_werkzeug=True)
