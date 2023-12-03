5fecc3f48906eff12fe201bfcb334ffc207880fc
GalaxyDestroyer <zagusopas25@gmail.com>
2023-12-03 19:29:47 +0800

    Try

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ justscanned = False
 socketio = SocketIO(app,cors_allowed_origins='*' )
 #fr_random_attendance_id = ""
 
-
+'''
 config = {
     "host": "roundhouse.proxy.rlwy.net",
     "user": "root",
@@ -37,7 +37,7 @@ config = {
     "password": "",
     "database": "zagusopass"
 }
-'''
+
 cnx = mysql.connector.connect(**config)
 mycursor = cnx.cursor(buffered=True)
 
@@ -116,6 +116,7 @@ global img_id,count_img,max_imgid
 count_img = 0
 
 def generate_dataset_socket(image):
+    frame = image
     face_classifier = cv2.CascadeClassifier("resources/haarcascade_frontalface_default.xml")
     '''
     mycursor.execute("select * from img_dataset WHERE img_person='" + str(nbr) + "'")
@@ -141,6 +142,7 @@ def generate_dataset_socket(image):
         for (x, y, w, h) in faces:
             cropped_face = img[y:y + h, x:x + w]
         return cropped_face
+
 
 
     '''
@@ -1354,7 +1356,7 @@ def report():
             "  from accs_hist a "
             "  left join users b on a.accs_prsn = b.id "
             "  left join tbl_groups c on a.group_id = c.id "
-            " where b.id != 0"
+            " where b.id != 0 and c.creater_id = " + str(user_id) +
             "" + str(filters) +
             " order by a.accs_id desc")
 
@@ -1965,7 +1967,6 @@ def receive_trainimage(image):
     processed_img_data = b64_src + processed_img_data
 
     emit("processed_trainimage", processed_img_data)
-
 
 
 if __name__ == "__main__":
