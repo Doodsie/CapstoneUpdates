58482d88098de809bce7604cfa17d82be06a4343
Doodsie <84783052+Doodsie@users.noreply.github.com>
2023-12-06 13:08:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ max_imgid = 1
 #socketio = SocketIO(engineio_logger=True, ping_timeout=5, ping_interval=5)
 #Payload.max_decode_packets = 50
 #socketio = SocketIO(async_mode='gevent', ping_timeout=cfg.service.PING_TIMEOUT, ping_interval=cfg.service.PING_INTERVAL)
-socketio = SocketIO(app,engineio_logger=True, ping_timeout=5, ping_interval=5 )
-Payload.max_decode_packets = 20
+socketio = SocketIO(app,cors_allowed_origins='*',engineio_logger=True, ping_timeout=5, ping_interval=5 )
+Payload.max_decode_packets = 50
 
 config = {
     "host": "roundhouse.proxy.rlwy.net",
